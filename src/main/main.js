0140e1c2877d86ddc96aeb26883e2aa792fbf440
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-04-18 12:39:07 +0530

    feat: add main process setup for Electron application

---
+++ src/main/main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { registerIpcHandlers } = require('./ipc');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 780,
+    minWidth: 980,
+    minHeight: 640,
+    backgroundColor: '#0b0d12',
+    titleBarStyle: 'hiddenInset',
+    webPreferences: {
+      preload: path.join(__dirname, '..', 'preload', 'index.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '..', '..', 'dist', 'renderer', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  registerIpcHandlers();
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
